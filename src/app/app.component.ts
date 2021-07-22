b47ea8d8c5a8c4dfd59e02530b628b2a9bce87fd
Valarmathi Shanmugam <valarmathishan@outlook.com>
2021-07-22 15:51:16 -0400

    with functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { ImortgageDetail } from 'src/app/shared/interface'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-Mortgage-Calculator';
+  isDetailAvailable = false;
+  mortgageDetails!: ImortgageDetail;
+
+  ngOnInit(): void {}
+
+  //Getting Obj value from the child component through EventEmitter
+  getMortageDetails = (obj: any) => {
+    this.isDetailAvailable = true;
+    this.mortgageDetails = Object.assign({}, obj);
+  };
 }
---
+++ src/app/component/mortgage-details/mortgage-details.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MortgageDetailsComponent } from './mortgage-details.component';
+
+describe('MortgageDetailsComponent', () => {
+  let component: MortgageDetailsComponent;
+  let fixture: ComponentFixture<MortgageDetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MortgageDetailsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MortgageDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
